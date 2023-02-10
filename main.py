9c4a919b91bbbef5891ae475681951250fe6c6f3
pablo <bedol.pab@gmail.com>
2023-02-09 22:12:02 -0600

    implemented neuron functionality. not error free

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
+import numpy as np
+from skimage import io
+from matplotlib import pyplot as plt
+from skimage import img_as_float
 import requests
 import shutil
-import numpy as np
-import 
+from io import BytesIO
+from PIL import Image
+
+class Neuron:
+    def __init__(self, data, neuron):
+        self.data = data
+        self.order = neuron
+
+    def __str__(self):
+        return f'Neuron {this.order} - Memory {this.data}'
 
 url = "https://thispersondoesnotexist.com/image"
-response  = requests.get(url, stream=True)
+image = requests.get(url).content
 
-print(response)
+with open('img.png', 'wb') as f:
+    f.write(image)
+
+image = io.imread("./img.png")
+plt.imshow(image)
+
+pixel_list = []
+count = 0
+for i in range(1023):
+    for j in range(1023):
+        pixel_list.append(image[i, j, :])
+        count += 1
+
+print(len(pixel_list))
+
+
+
